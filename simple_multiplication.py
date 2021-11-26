7f85022a9da77520ee5c04761d129a6d129015d1
Saju Pillai <saju.pillai@gmail.com>
2021-11-26 13:13:23 -0800

    basic Nx = M class of equations

---
+++ simple_multiplication.py
@@ -0,0 +1,32 @@
+import random
+
+#
+# python2 simple_multiplication.py
+#
+# Generates 10 questions of the form Ny = M, where both N & M are small numbers.
+# 
+# How I use this program..
+# python2 simple_multiplication.py > qns_plus_answers.txt
+# cat qns_plus_answers.txt | grep -v answer > qns.txt
+# ... then printout qns.txt and hand over to the child :)
+#
+
+MAX_N = 5
+MAX_M = 10
+
+def gen_simple_mult(num_questions):
+    for i in range(num_questions):
+        mult = random.randint(1, MAX_N + 1)
+        y = random.randint(1, MAX_M + 1)
+        print "Question %d" % i
+        print "%dy = %d" % (mult, mult * y)
+        print "y = ?"
+        print "answer %d" % y
+        print "**************************"
+
+def parse_arguments():
+    pass
+
+if __name__ == "__main__":
+    parse_arguments()
+    gen_simple_mult(num_questions = 10)
